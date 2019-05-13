2ef6aa2c5c4542228d1bbee8e09acb0f04f89152
Devian <forza9882@gmail.com>
2019-05-13 07:42:57 +0200

    fixing Mismatched text domain. Expected 'kappscores2.1_themes' but got 'kappscores'.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package kappscores
+ * @package kappscores2.1_themes
  */
 
 ?>
@@ -18,7 +18,7 @@
     if ( has_post_thumbnail() ) { ?>
         <figure class="featured-image full-bleed">
             <?php
-            the_post_thumbnail('kappscores-full-bleed');
+            the_post_thumbnail('kappscores2.1_themes-full-bleed');
             ?>
         </figure><!-- .featured-image full-bleed -->
     <?php } ?>
@@ -29,7 +29,7 @@
         the_content();
 
         wp_link_pages( array(
-            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kappscores' ),
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kappscores2.1_themes' ),
             'after'  => '</div>',
         ) );
         ?>
